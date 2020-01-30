c28bc3971757d74165661fa1d181126435ad354f
maerd19 <revolutions_333@hotmail.com>
2020-01-30 05:04:52 -0600

    configuracion prod

--- react/webpack-4/platzi-badges/webpack.config.js
+++ react/webpack-4/platzi-badges/webpack.config.js
@@ -2,17 +2,16 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 
 module.exports = {
     entry: {
-        home: path.resolve(__dirname, 'src/js/index.js'),
-        contact: path.resolve(__dirname, 'src/js/contact.js'),
+        app: path.resolve(__dirname, 'src/index.js'),
     },
-    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js',
-        publicPath: 'dist/',
+        publicPath: 'http://localhost:3001/',
         chunkFilename: 'js/[].[chunkhash].js'
     },
     module: {
@@ -28,52 +27,16 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader
                     },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    'postcss-loader',
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
                     'css-loader',
-                    'less-loader'
                 ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader',
-                    'stylus-loader'
-                ]
-            },
+            },            
             // Este loader servira para darle soporte a todos los archivos con los siguientes tipos de extensiones
             {
                 test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
                 use: {
                     loader: 'url-loader',
                     options: {
-                        limit: 90000,
+                        limit: 1000,
                     }
                 },                
             }
@@ -85,14 +48,17 @@ module.exports = {
             filename: 'css/[name].css',
             chunkFilename: 'css/[id].css'
         }),
-        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
-            title: 'webpack-dev-server',
-            template: path.resolve(__dirname, 'index.html'),
+            template: path.resolve(__dirname, 'public/index.html'),
         }),
         new webpack.DllReferencePlugin({
             // Este archivo se creara cuando se corra DllPlugin
             manifest: require('./modules-manifest.json')
+        }),
+        new AddAssetHtmlPlugin({
+            filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
+            outputPath: 'js',
+            publicPath: 'http://localhost:3001/'
         })
     ]
 }
