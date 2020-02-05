43045cb7b30c2a46027008e0f491200d2f33aa37
maerd19 <revolutions_333@hotmail.com>
2020-02-05 16:01:41 -0600

    imports, variables y fuentes de Google en Sass

--- react/PlatziVideo/webpack.config.js
+++ react/PlatziVideo/webpack.config.js
@@ -38,8 +38,18 @@ module.exports = {
                     },
                     'css-loader',
                     'sass-loader'
+                ]                
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        'loader': 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]'
+                        }
+                    }
                 ]
-                
             }
         ]
     },
