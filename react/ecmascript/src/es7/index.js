accc1b1990cfd3e19370f7bb2aa05f4307656b9a
maerd19 <revolutions_333@hotmail.com>
2020-03-06 17:21:43 -0600

    ES7

---
+++ react/ecmascript/src/es7/index.js
@@ -0,0 +1,15 @@
+// Nace en junio de 2016
+// Includes: Trabaja sobre un arreglo o string y nos permitira saber si hay un elemento dentro de esta estructura.
+// Anteriormente se utilizaba indexOf
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(9)) {
+    console.log('Si se encuentra el valor 7');    
+} else {
+    console.log('No se encuentra');    
+}
+// Operaciones en forma exponencial
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
