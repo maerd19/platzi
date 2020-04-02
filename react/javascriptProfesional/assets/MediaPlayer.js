71f60fcde600675f15940a5f1b638800d72b4f4b
maerd19 <revolutions_333@hotmail.com>
2020-04-02 13:07:30 -0600

    this

--- react/javascriptProfesional/assets/MediaPlayer.js
+++ react/javascriptProfesional/assets/MediaPlayer.js
@@ -4,9 +4,18 @@ function MediaPlayer(config) {
     // El objeto tiene una propiedad que almacenara el contenido del parametro
     // this guarda un valor en la instancia
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
 
 // Metodos de MediaPlayer
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this)
+    })
+}
+
 MediaPlayer.prototype.togglePlay = function() {
     (this.media.paused) ? this.play() : this.pause();
 };      
@@ -20,5 +29,19 @@ MediaPlayer.prototype.play = function() {
 MediaPlayer.prototype.pause = function() {
     this.media.pause();
 };
+
+MediaPlayer.prototype.toggleMute = function() {
+    (this.media.muted) ? this.unmute() : this.mute();
+}
+
+// Metodo de mute
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+}
+
+// Metodo de unmute
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+}
 
 export default MediaPlayer;
--- react/javascriptProfesional/assets/index.js
+++ react/javascriptProfesional/assets/index.js
@@ -1,10 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
-
+import AutoPlay from './plugins/AutoPlay.js';
 // querySelector toma un elemento para poder manipularlo
 const video = document.querySelector('video');
 // Mediante el player exponemos los metodos de la clase
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [ 
+    // new AutoPlay() 
+] });
 
-// Boton que tendra funcionalidad
+// Boton de reproduccion/pausa
 const button = document.querySelector('button');
 button.onclick = () => player.togglePlay();
+
+// Boton de mute/unmute
+const mute = document.getElementById('mute');
+mute.onclick = () => player.toggleMute();
